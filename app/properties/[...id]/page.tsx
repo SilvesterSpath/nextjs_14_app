c0f87cc12c5b075fd981564717d2087fd4d518b6
SilvesterSpath <spath.sz@gmail.com>
2024-05-24 12:39:29 +0200

    next/navigation

--- app/properties/[...id]/page.tsx
+++ app/properties/[...id]/page.tsx
@@ -1,12 +1,30 @@
 'use client';
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from 'next/navigation';
 
 const PropertyPage = () => {
   const router = useRouter();
+  const { id } = useParams();
+  const searchParams = useSearchParams();
+  const name = searchParams.get('name');
+  const pathname = usePathname();
 
-  console.log('Hello from the console');
-  return <div>PropertyPage</div>;
+  console.log('Hello from the browser');
+  return (
+    <div>
+      <button onClick={() => router.push('/')} className='bg-blue-400 p-2'>
+        Go Home
+      </button>
+      <p className='text-2xl p-3 mx-auto'>
+        {id}, {name}, {pathname}
+      </p>
+    </div>
+  );
 };
 
 export default PropertyPage;
