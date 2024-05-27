61728fb93551173f32bf50317bf25d30637fd6e9
SilvesterSpath <spath.sz@gmail.com>
2024-05-27 06:38:45 +0200

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import Image from 'next/image';
+import logo from '@/assets/images/logo-white.png';
+import profileDefault from '@/assets/images/profile.png';
 
 const Navbar = () => {
   return (
@@ -36,11 +38,7 @@ const Navbar = () => {
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
             {/* <!-- Logo --> */}
             <a className='flex flex-shrink-0 items-center' href='/index.html'>
-              <img
-                className='h-10 w-auto'
-                src='images/logo-white.png'
-                alt='PropertyPulse'
-              />
+              <Image className='h-10 w-auto' src={logo} alt='PropertyPulse' />
 
               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
                 PropertyPulse
@@ -122,9 +120,9 @@ const Navbar = () => {
                 >
                   <span className='absolute -inset-1.5'></span>
                   <span className='sr-only'>Open user menu</span>
-                  <img
+                  <Image
                     className='h-8 w-8 rounded-full'
-                    src='images/profile.png'
+                    src={profileDefault}
                     alt=''
                   />
                 </button>
